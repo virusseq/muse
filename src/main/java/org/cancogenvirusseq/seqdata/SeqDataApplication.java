7c0c9545d78818130122b775a0bf5685c46ede19
jaserud <udjaser@gmail.com>
2021-03-26 15:07:21 -0400

    initialize service (#1)

---
+++ src/main/java/org/cancogenvirusseq/seqdata/SeqDataApplication.java
@@ -0,0 +1,13 @@
+package org.cancogenvirusseq.seqdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeqDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeqDataApplication.class, args);
+	}
+
+}
