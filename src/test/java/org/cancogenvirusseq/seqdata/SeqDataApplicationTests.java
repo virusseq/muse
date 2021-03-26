6af7b8e415e20a64e120f1dcf106ec9d3abe8278
jaserud <udjaser@gmail.com>
2021-03-26 11:27:23 -0400

    initialize service

---
+++ src/test/java/org/cancogenvirusseq/seqdata/SeqDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cancogenvirusseq.seqdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeqDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
