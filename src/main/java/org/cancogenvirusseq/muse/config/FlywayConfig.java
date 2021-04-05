da94ef13b207c16e0242b1d8dbf85e500b3f2da0
Dusan Andric <dusan.andric@gmail.com>
2021-04-05 15:37:09 -0400

    formatting

--- src/main/java/org/cancogenvirusseq/muse/config/FlywayConfig.java
+++ src/main/java/org/cancogenvirusseq/muse/config/FlywayConfig.java
@@ -2,8 +2,6 @@ package org.cancogenvirusseq.muse.config;
 
 import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.flyway.FlywayProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
@@ -18,14 +16,11 @@ public class FlywayConfig {
   }
 
   @Bean(initMethod = "migrate")
-  public Flyway flyway(@Value("${spring.flyway.url}") String url,
-                       @Value("${spring.flyway.username}") String username,
-                       @Value("${spring.flyway.password}") String password) {
+  public Flyway flyway(
+      @Value("${spring.flyway.url}") String url,
+      @Value("${spring.flyway.username}") String username,
+      @Value("${spring.flyway.password}") String password) {
 
-    return new Flyway(
-        Flyway.configure()
-            .dataSource(
-              url, username, password));
+    return new Flyway(Flyway.configure().dataSource(url, username, password));
   }
-
 }
