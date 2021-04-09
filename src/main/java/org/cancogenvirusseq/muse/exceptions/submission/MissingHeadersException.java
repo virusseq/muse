6c02e20406ff86e449684ca91cd18c708c7dd745
Alex <lepsalex@gmail.com>
2021-04-09 15:57:44 -0400

    songScore service onErrorResume tweak

--- src/main/java/org/cancogenvirusseq/muse/exceptions/submission/MissingHeadersException.java
+++ src/main/java/org/cancogenvirusseq/muse/exceptions/submission/MissingHeadersException.java
@@ -1,11 +1,14 @@
 package org.cancogenvirusseq.muse.exceptions.submission;
 
-import java.util.List;
-import java.util.Map;
+import lombok.EqualsAndHashCode;
 import lombok.Value;
 import org.cancogenvirusseq.muse.exceptions.MuseBaseException;
 
+import java.util.List;
+import java.util.Map;
+
 @Value
+@EqualsAndHashCode(callSuper = true)
 public class MissingHeadersException extends Throwable implements MuseBaseException {
   List<String> missingHeaders;
   List<String> unknownHeaders;
